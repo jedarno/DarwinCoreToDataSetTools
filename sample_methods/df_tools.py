c819aacc478d5b06b6242d8be5a3ea19cedbc6ce
Jed Arno <jedarno99@gmail.com>
2025-02-04 14:52:00 +0000

    Getting Freq infomation

--- sample_methods/df_tools.py
+++ sample_methods/df_tools.py
@@ -3,15 +3,27 @@ import os
 import urllib.request
 
 def observations_to_df(observations_file: str, multimedia_file: str, delim: str = '\t', save_to: str = None) -> pd.core.frame.DataFrame:
+
   occurence_table = pd.read_csv(observations_file, delimiter=delim)
   media = pd.read_csv(multimedia_file, delimiter=delim)
   taxa = occurence_table[["gbifID","family","genus","species"]]
   images = media[["gbifID","identifier"]]
   data = pd.merge(images,taxa,on="gbifID", how="inner")
   data = data.dropna()
+
   if save_to != None:
-    try:
-      data.to_csv(save_to)
+    data.to_csv(save_to)
+
   return data
 
+def get_taxa_freq(taxa_data_frame: pd.core.frame.DataFrame | str):
+  
+  if type(taxa_data_frame == str):
+    taxa_data_frame = pd.read_csv(taxa_data_frame)
 
+  families_freq = taxa_data_frame['family'].value_counts(sort=True)
+  genera_freq = taxa_data_frame['genus'].value_counts(sort=True)
+  species_freq = taxa_data_frame['species'].value_counts(sort=True)
+  families_freq.to_csv('./freq_tables/families_freq.csv')
+  genera_freq.to_csv('./freq_tables/genera_freq.csv')
+  species_freq.to_csv('./freq_tables/species_freq.csv')
