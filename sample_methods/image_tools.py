868e03de0a7bbd4667d09e47fe03a5e76bb0da99
Jed Arno <jedarno99@gmail.com>
2025-02-06 17:09:49 +0000

    Downloading sample image links into directory

--- sample_methods/image_tools.py
+++ sample_methods/image_tools.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 def build_image_dir(labels: list, folder_location: str = "images"):
   """
@@ -15,5 +16,64 @@ def build_image_dir(labels: list, folder_location: str = "images"):
       os.makedirs(folder_location + "/train/" + label)
     if not os.path.exists(folder_location + "/test/" + label):
       os.makedirs(folder_location + "/test/" + label)
+
+def download_image(image_link: str, path: str):
+  """
+  Download image from a link. The image is resized to 1090 x 1600. 
+  """
+  img_data = requests.get(image_link, timeout=5).content
+
+  with open(path, 'wb') as handler:
+    handler.write(img_data)
+
+  #resize image
+  image = cv2.imread(path)
+  image = cv2.resize(image, (1090, 1600))
+  cv2.imwrite(path, image)
+
+def download_sample(directory, sample_df, label_id, replacement_df):
+  """
+  Donwload image links from sample into the dataset directory
+  """
+
+  broken_links = []
+  missing_images = []
+  labels = sample_df[label_id].to_list()
+  gbif_ids = sample_df['gbifID'].to_list()
+  image_links = sample_df['identifier'].to_list()
+  
+  for i in range(0, len(labels)):
+    class_folder = labels[i]
+    image_name = "image-{}-{}".format(gbif_ids[i], i)
+    path = "train/{}/{}.png".format(class_folder, image_name)
+
+    try:
+      download_image(image_links[i], path)
+    except Exception as e:
+      broken_links.append(i)
+      replaced = False
+      alternative_images = replacement_df[ (replacement_df[label_id] == labels[i]) & (~replacement_df['gbifID'].isin(gbif_ids)) ]
+      
+      if len(alternative_imags > 0):
+
+        for j in range (0, len(alternative_images)):
+
+          try:
+            alt_image_name = "image-{}-{}".format(alternative_images.iloc[j]['gbifID'], j)
+            alt_path = "train/{}/{}.png".format(class_folder, alt_image_name)
+            download_image(alternative_images.iloc[j]['identifier'], alt_path)
+            replaced = True
+            break
+
+          except Exception as e:
+            print("Exception {} at link {}".format(e, alternatve_images.iloc[j]['identifier']))
+
+        if not replaced:
+          missing_images.append(labels[i])
+
+      else:
+        missing_images.append(labels[i])
+
+
 
 
